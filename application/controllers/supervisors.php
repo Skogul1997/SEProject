b3ef75455059228965cdd67acefa02873044d7a1
JoblessKogul <46989547+JoblessKogul@users.noreply.github.com>
2019-11-10 10:40:07 +0530

    Initial commit with overall project structure

--- application/controllers/supervisors.php
+++ application/controllers/supervisors.php
@@ -1,7 +1,8 @@
 <?php
 
 	class supervisors extends CI_Controller{
-		
+
+		// Function to view pages inside the supervisor view
 		public function view ($page = 'index'){
 			if(!file_exists(APPPATH.'views/supervisors/'.$page.'.php')){
 					show_404();
@@ -12,6 +13,7 @@
 			$this->load->view('templates/footer');
 		}
 
+		// Function to view the default view which is the index view
 		public function index(){
 			$data['title'] = 'Sign in as Supervisor';
 			$this->load->view('templates/header');
@@ -19,7 +21,7 @@
 			$this->load->view('templates/footer');
 		}
 
-		// Login supervisor
+		// Supervisor Login function
 		public function login(){
 			$data['title'] = 'Sign In';
 			$this->form_validation->set_rules('username' , 'Username' , 'required');
@@ -31,7 +33,7 @@
 			} else {
 				$username = $this->input->post('username');
 				$enc_password = md5($this->input->post('password'));
-				//Login user
+				// Calling function of supervisor model to check login credentials (returns supervisor_id)
 				$supervisor_id = $this->supervisor_model->login($username,$enc_password);
 				if($supervisor_id){
 					$this->session->set_flashdata('login_success','You are now logged in');
@@ -44,7 +46,7 @@
 			}
 		}
 
-		//Add Trainee
+		// Adding Trainees to database using this military identification number as username and password
 		public function addTrainee(){
 			$data['title'] = 'Add Trainee';
 			$this->form_validation->set_rules('username','Username','required');
@@ -56,8 +58,10 @@
 				$this->load->view('templates/footer');
 			} else {
 				$username = $this->input->post('username');
+				// Hashing password using md5 algorithm
 				$enc_password = md5($username);
 				$designation = $this->input->post('designation');
+				// Call to supervisor_model to execute the addTrainee function
 				if($this->supervisor_model->addTrainee($username,$enc_password,$designation)){
 					$this->session->set_flashdata('add_trainee', 'Trainee with ID '.$username.' has been successfully added.');
 					redirect('supervisors/add_Trainee');
