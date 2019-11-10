e87906be6578f580b10a71ae6e81117e5acdfc3f
JoblessKogul <kogul.17@cse.mrt.ac.lk>
2019-11-10 13:55:07 +0530

    training view added and scripts updated for routing.

--- application/views/supervisors/home.php
+++ application/views/supervisors/home.php
@@ -1,4 +1,5 @@
 <a href='addTrainee'>Add Trainee</a><br>
 <a href='viewTrainees'>View Trainees</a><br>
 <a href='addSupervisor'>Add Supervisor</a><br>
+<a href='training'>Training</a><br>
 <a href='logout'>Logout</a><br>
