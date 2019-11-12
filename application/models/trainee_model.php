6916896755183e38ea607376e421e8e1ae5cddca
Vihanga97 <vihanga.dewmini.97@gmail.com>
2019-11-12 12:13:00 +0530

    updated trainee model

--- application/models/trainee_model.php
+++ application/models/trainee_model.php
@@ -2,7 +2,30 @@
 	
 	class trainee_model extends CI_Model{
 
-		public function __construct{
-			
+		public function __construct(){
+			$this->load->database();
 		}
+		public function login($username,$password){
+			//Validate
+			$this->db->where('username',$username);
+			$this->db->where('password',$password);
+			$result = $this->db->get('supervisors');
+			if($result->num_rows() == 1){
+				return $result->row(0)->id;
+			} else {
+				return FALSE;
+			}
+		}
+		
+		public function getTrainingDetails(){
+			$query = $this->db->get('training');
+			return $query->result_array();
+		}
+
+		public function changeStatus($trainee_id){
+			$this->db->set('status', 'In-Training');
+			$this->db->where('trainee_id', $trainee_id);
+			$this->db->update('training');
+		}
+
 	}
