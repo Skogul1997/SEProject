ceaf0ce2387b272e2942a51324db55081db157ec
JoblessKogul <kogul.17@cse.mrt.ac.lk>
2019-11-12 07:07:35 +0530

    Did small changes in the supervisors views.

--- application/views/supervisors/training.php
+++ application/views/supervisors/training.php
@@ -1,12 +1,14 @@
 <?php if(count($trainings)==0){
 	echo 'No Trainees Available.';
 } else 		{foreach($trainings as $training):
-				echo $training['trainee_id'].' '.$training['status'].' ';
+				echo $training['trainee_id'].'<br>'.$training['status'].'<br>';
 				$trainee_id = $training['trainee_id'];
 				if($training['status']!='In-Training'){
 					echo "<a href=changeStatus?id=".$trainee_id."><button type='button'>Train</button></a>";
 				} else {
 					echo "<a href=''><button type='button' disabled>Train</button></a>";
 				}
+				echo "<br><br>";
 			endforeach; } ?>
+<br>
 <a href='logout'>Logout</a><br>
